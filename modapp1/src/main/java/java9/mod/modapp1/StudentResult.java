d9d13725931792dcaf56f1692ab1be8c35bd5c41
Administrator <Administrator@sumedha-PC>
2020-09-10 17:54:01 +0530

    Updated and uploaded

--- modapp1/src/main/java/java9/mod/modapp1/StudentResult.java
+++ modapp1/src/main/java/java9/mod/modapp1/StudentResult.java
@@ -1,24 +1,89 @@
 package java9.mod.modapp1;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
+
 public class StudentResult {
 	private String rollNumber;
 	private String studentName;
 	private Double halfYearlyTotal;
 	private Double annualTotal;
 	private String grade;
+	
+	public StudentResult() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public StudentResult(String rollNumber, String studentName, Double halfYearlyTotal, Double annualTotal,
+			String grade) {
+		super();
+		this.rollNumber = rollNumber;
+		this.studentName = studentName;
+		this.halfYearlyTotal = halfYearlyTotal;
+		this.annualTotal = annualTotal;
+		this.grade = grade;
+	}
+
+
 	public StudentResult(String rollNumber, String studentName, Double halfYearlyTotal) {
 		super();
 		this.rollNumber = rollNumber;
 		this.studentName = studentName;
 		this.halfYearlyTotal = halfYearlyTotal;
 	}
+	
+	public String getRollNumber() {
+		return rollNumber;
+	}
+
+
+	public void setRollNumber(String rollNumber) {
+		this.rollNumber = rollNumber;
+	}
+
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+
+	public Double getHalfYearlyTotal() {
+		return halfYearlyTotal;
+	}
+
+
+	public void setHalfYearlyTotal(Double halfYearlyTotal) {
+		this.halfYearlyTotal = halfYearlyTotal;
+	}
+
+
+	public Double getAnnualTotal() {
+		return annualTotal;
+	}
+
+
+	public void setAnnualTotal(Double annualTotal) {
+		this.annualTotal = annualTotal;
+	}
+
+
+	public String getGrade() {
+		return grade;
+	}
+
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+
 	@Override
 	public String toString() {
 		String output=String.format("%-5s %-20s %-20s %-20s %5s",rollNumber,studentName,halfYearlyTotal,annualTotal,grade);
